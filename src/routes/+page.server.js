d4916cb7a00c2d87227d512970a4d81ff23c69ec
Olek Osikowicz <tupolewcx@gmail.com>
2023-11-06 22:14:49 +0000

    Fetch requests from remote server

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,9 +3,13 @@
 export const load = async ({ fetch }) => {
     // const res = await fetch('api/maps')
     // const item = await res.json()
-    const res = await fetch('warsaw_emerald_a3.svg')
-    const item = await res.text()
+    // const res = await fetch('warsaw_emerald_a3.svg')
 
-    return { svg_string: item }
+    const res = await fetch('http://0.0.0.0:8000/v1/map')
+
+    // const item = await res.text()
+    const item = await res.json()
+
+    return { svg_string: item.svg_string }
 
 }
