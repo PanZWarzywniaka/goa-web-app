03bc465a86cf7f3ec09f4d4b258db1638c1ced2b
Olek Osikowicz <tupolewcx@gmail.com>
2023-11-21 20:17:48 +0000

    Searching location with debounce plus simplification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,53 @@
-<script>
-	import { createSearchStore, searchHandler } from '$lib/stores/search'
-	import { onDestroy } from 'svelte'
-	// let location = 'New York'
+<script lang="ts">
+	let foundSuggestions: string[] = []
 
-	const searchStore = createSearchStore()
+	const searchLocations = async (event: KeyboardEvent) => {
+		const input = event.target as HTMLInputElement
+		const query = input.value
 
-	const unsubscribe = searchStore.subscribe((model) => searchHandler(model))
+		if (!query || query.length === 0) {
+			foundSuggestions = []
+			return
+		}
+		const url = `https://nominatim.openstreetmap.org/search?q=${query}&limit=10&format=json`
+		const response = await (await fetch(url)).json()
 
-	onDestroy(() => {
-		unsubscribe()
-	})
+		console.log(response)
+		foundSuggestions = response.map((el) => {
+			return el.display_name
+		})
+	}
+
+	const debounce = (callback: Function, wait = 300) => {
+		let timeout: ReturnType<typeof setTimeout>
+
+		return (...args: any[]) => {
+			clearTimeout(timeout)
+			timeout = setTimeout(() => callback(...args), wait)
+		}
+	}
+
+	$: console.log(foundSuggestions)
 </script>
 
 <div class="col-lg-6 col-md-8 mx-auto">
 	<h1>Hello</h1>
-	<input type="search" bind:value={$searchStore.search} />
-	{#each $searchStore.suggestions as s}
-		<p>{s}</p>
-	{/each}
-	<!-- <a class="btn" href="/map/{location}">Click here to generate {location}</a> -->
+	<form>
+		<input
+			name="search"
+			type="search"
+			placeholder="Search location"
+			on:keyup={debounce(searchLocations)}
+		/>
+
+		{#if foundSuggestions.length > 0}
+			<ul>
+				{#each foundSuggestions as suggestion}
+					<li class="flex px-4 py-2 border border-gray-200 bg-gray-50 hover:bg-gray-200">
+						{suggestion}
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</form>
 </div>
