b978f120f9c3cb8aa678970c013c0d56cd403552
Olek Osikowicz <tupolewcx@gmail.com>
2023-12-07 18:03:36 +0000

    Removing .firebase from repo

--- .firebase/goa-app-9b793/functions/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.df69d96f.js","_app/immutable/chunks/scheduler.17b940cc.js","_app/immutable/chunks/index.fa92f6d7.js","_app/immutable/chunks/singletons.27faa56f.js","_app/immutable/chunks/index.953680d5.js"];
-export const stylesheets = [];
-export const fonts = [];
