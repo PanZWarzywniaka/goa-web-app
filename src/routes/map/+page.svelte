4618ced00d6a7b5f4426be3a75c2b1b1585703ca
Olek Osikowicz <tupolewcx@gmail.com>
2024-01-02 13:01:21 +0000

    Add more colour pickers for each layer

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -6,19 +6,33 @@
 	export let data
 
 	let land_hex = '#ffffff'
-	let isMounted = false
 
-	$: if (isMounted) {
-		console.log(`Switching colour to ${land_hex}`)
-		// @ts-ignore
-		document.getElementById('land').attributes.fill.value = land_hex
+	let colours = {
+		land: '#ffffff',
+		water: '#aaaaff',
+		green: '#00ff00'
 	}
 
+	let isMounted = false
+
+	//initialize
 	onMount(() => {
-		let bg = document.getElementById('land')
-		land_hex = bg.attributes.fill.value
+		colours.land = document.getElementById('land')?.attributes.fill.value
+		colours.water = document.getElementById('water')?.attributes.fill.value
+		colours.green = document.getElementById('green')?.attributes.fill.value
+
+		console.log(`Colours initialized to: \n ${JSON.stringify(colours)}`)
 		isMounted = true
 	})
+
+	// on every change
+	$: if (isMounted) {
+		console.log(`Switching colours to: \n ${JSON.stringify(colours)}`)
+
+		document.getElementById('land').attributes.fill.value = colours.land
+		document.getElementById('water').attributes.fill.value = colours.water
+		document.getElementById('green').attributes.fill.value = colours.green
+	}
 </script>
 
 <div class="col-8">
@@ -30,7 +44,9 @@
 </div>
 
 <div class="col">
-	<ColorPicker bind:hex={land_hex} label="Land cover" isAlpha={false} canChangeMode={false} />
+	<ColorPicker bind:hex={colours.land} label="Land cover" isAlpha={false} canChangeMode={false} />
+	<ColorPicker bind:hex={colours.water} label="Water" isAlpha={false} canChangeMode={false} />
+	<ColorPicker bind:hex={colours.green} label="Greenery" isAlpha={false} canChangeMode={false} />
 </div>
 
 <style>
