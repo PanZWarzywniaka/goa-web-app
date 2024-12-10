6203840fe1bca7e05335dce300cf723c0f17f0f6
Olek Osikowicz <tupolewcx@gmail.com>
2024-12-10 21:38:13 +0000

    Use the new API_URL env variable to define remote URL

--- src/lib/server/util.ts
+++ src/lib/server/util.ts
@@ -1,28 +1,26 @@
-import { env } from "$env/dynamic/private"
+import { env } from '$env/dynamic/private'
 
 export function getAPIEndpoint(): URL {
+	let api_url = new URL(`http://0.0.0.0:8000`)
+	if (env.API_URL) {
+		api_url = new URL(env.API_URL)
+	}
 
-    let api_url = new URL(`https://goa.olek.site`)
-    if (env.LOCAL == "true") {
-        api_url = new URL(`http://0.0.0.0:8000`)
-    }
-    console.debug(`API URL: ${api_url}`)
-    return api_url
+	console.log(`API URL: ${api_url}`)
+	return api_url
 }
 
-
 export function getRequestOptions(body: string) {
+	const headers = {
+		Accept: 'application/json',
+		'Content-Type': 'application/json'
+	}
 
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    };
+	const requestOptions = {
+		method: 'POST',
+		headers: headers,
+		body: body
+	}
 
-    const requestOptions = {
-        method: 'POST',
-        headers: headers,
-        body: body
-    }
-
-    return requestOptions
+	return requestOptions
 }
